f7185e3d9c73dab154249283bbdb9c5846bebeda
gamaandersonnn <skazzbr@hotmail.com>
2026-03-02 22:09:40 -0300

    implementando login com google

--- SpringJWT + Oauth2-CRUD-SpringBoot3/src/main/java/com/example/Spring_JWT_CRUD/configs/SecurityConfiguration.java
+++ SpringJWT + Oauth2-CRUD-SpringBoot3/src/main/java/com/example/Spring_JWT_CRUD/configs/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.Spring_JWT_CRUD.configs;
 
+import com.example.Spring_JWT_CRUD.entities.User;
+import com.example.Spring_JWT_CRUD.repositories.UserRepository;
+import com.example.Spring_JWT_CRUD.services.JwtService;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -20,13 +24,19 @@ import java.util.List;
 public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
 
     public SecurityConfiguration(
             AuthenticationProvider authenticationProvider,
-            JwtAuthenticationFilter jwtAuthenticationFilter
+            JwtAuthenticationFilter jwtAuthenticationFilter,
+            UserRepository userRepository,
+            JwtService jwtService
     ){
         this.authenticationProvider = authenticationProvider;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
     }
 
     @Bean
@@ -37,9 +47,29 @@ public class SecurityConfiguration {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/login/**").permitAll()
                         .anyRequest().authenticated()
                 )
+
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler((request, response, authentication) -> {
+                            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+                            String email = oAuth2User.getAttribute("email");
+                            String name = oAuth2User.getAttribute("name");
+
+                            User user = userRepository.findByEmail(email)
+                                    .orElseGet(() -> {
+                                        User newUser = new User();
+                                        newUser.setFullName(name);
+                                        newUser.setEmail(email);
+                                        newUser.setPassword("");
+                                        return userRepository.save(newUser);
+                                    });
+
+                            String token = jwtService.generateToken(user);
+
+                            response.sendRedirect("http://127.0.0.1:5500/usuarios.html?token=" + token);
+                        }))
 
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
